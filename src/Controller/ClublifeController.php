bfea7754aa00b50c2dc51b0cfa873be89d9e4c42
Olivier <olivier.monsire@gmail.com>
2020-05-06 17:57:19 +0200

    recreate admin clublife edit

--- src/Controller/ClublifeController.php
+++ src/Controller/ClublifeController.php
@@ -9,7 +9,10 @@
 
 namespace App\Controller;
 
+use App\Model\ClublifeManager;
 use App\Model\EventManager;
+use App\Model\ExhibitionManager;
+use App\Verify\VerifyFileUpload;
 
 /**
  * Class contactController
@@ -29,6 +32,63 @@ class ClublifeController extends AbstractController
      */
     public function index()
     {
-        return $this->twig->render('Clublife/index.html.twig');
+        $clublifeManager = new ClublifeManager();
+        $clublifes = $clublifeManager->selectClublife();
+        return $this->twig->render('Clublife/index.html.twig', ['clublifes' => $clublifes]);
+    }
+
+    public function update(): array
+    {
+        $data = array_map('trim', $_POST);
+        $errors = $this->controlData($data);
+
+        if (empty($errors)) {
+            $clublifeManager = new clublifeManager();
+            $clublifeManager->update($data);
+            foreach ($_FILES as $name => $image) {
+                if ($image['error'] != 4) {
+                    $image['position'] = $name;
+                    $files = new VerifyFileUpload($_FILES);
+                    $data = array_map('trim', $image);
+                    $upload = $files->fileControl(true);
+                    if (isset($upload["$name"])) {
+                        if (isset($upload["$name"]['message code'])) {
+                            $clublifeManager->updateimg($data);
+                            header('Location: /admin/clublife/?success=Modifications appliquées');
+                        } else {
+                            header('Location: /admin/clublife/?error=Une erreur est survenue');
+                        }
+                    } else {
+                        header('Location: /admin/clublife/?error=Une erreur est survenue');
+                    }
+                }
+            }
+        }
+        return $errors;
+    }
+
+    private function controlData($data): array
+    {
+        $errors = [];
+
+        foreach ($data as $name => $value) {
+            $convert = [
+                'description_title' => 'titre de la description',
+                'description' => 'description',
+                'activity_title' => 'titre des activités',
+                'activity' => 'activités'
+            ];
+
+            if (empty($value)) {
+                $errors[] = 'Le champ ' . $convert[$name] . ' est requis';
+            }
+            if ($name == 'description_title' && strlen($value) > 100) {
+                $errors[] = 'Le champ ' . $convert[$name] . ' doit être inférieur à 100 caractères';
+            }
+            if ($name == 'activity_title' && strlen($value) > 100) {
+                $errors[] = 'Le champ ' . $convert[$name] . ' doit être inférieur à 100 caractères';
+            }
+        }
+        return $errors;
     }
 }
