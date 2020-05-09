849b222c30bc80113a8cd1965751164670de168c
Julien <eoz.wild@gmail.com>
2020-05-09 13:09:35 +0200

    Added form and date control

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -38,23 +38,73 @@ class EventController extends AbstractController
 
     public function add()
     {
+        $eventManager = new EventManager();
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $destination = 'assets/images/events/';
             $files = new VerifyFileUpload($_FILES);
             $data = array_map('trim', $_POST);
+            $errors = $this->formControl($data);
+            $errorsDate = $this->dateControl($data);
             $upload = $files->fileControl(true);
 
-            if (empty($errors)) {
-                if (!empty($upload['img'])) {
-                    $data['img'] = $upload['img']['name'];
-                    $files->uploadFile($upload['img']['tmp_name'], $destination, $upload['img']['name']);
+            if (empty($errors) && empty($errorsDate)) {
+                if (!empty($upload['image'])) {
+                    $data['image'] = $upload['image']['name'];
+                    $files->uploadFile($upload['image']['tmp_name'], $destination, $upload['image']['name']);
                 }
+                $eventManager->add($data);
                 header('Location: /admin/event/?success=Évènement ajouté');
             }
         }
 
-        return $this->twig->render('Admin/Add/event.html.twig', ['errors' => $errors ?? []
+        return $this->twig->render('Admin/Add/event.html.twig', [
+            'errors' => $errors ?? [],
+            'errorsDate' => $errorsDate ?? [],
         ]);
+    }
+
+    private function formControl($data): array
+    {
+        $errors = [];
+
+        if (strlen($data['name']) > 255) {
+            $errors['length_name'] = 'Le nom de l\'évènement doit contenir 255 caractères au maximum';
+        }
+
+        if (empty($data['name'])) {
+            $errors['empty_name']  = 'Le nom de l\'évènement est requis';
+        }
+
+        if (empty($data['start_date'])) {
+            $errors['empty_start_date'] = 'La date de début de l\'évènement est requis';
+        }
+
+        if (empty($data['image'])) {
+            $errors['empty_image'] = 'Une image est requise';
+        }
+        return $errors;
+    }
+
+    private function dateControl($data): array
+    {
+        $errorsDate = [];
+
+        if ($data['start_date'] < date("Y-m-d")) {
+            $errorsDate['early_start_date'] =
+                "La date de début de l\'évènement doit être égale au minimum à la date du jour";
+        }
+
+        if ($data['end_date'] < date("Y-m-d")) {
+            $errorsDate['early_end_date'] =
+                "La date de fin de l\'évènement doit être égale au minimum à la date du jour";
+        }
+
+        if ($data['end_date'] <= $data['start_date']) {
+            $errorsDate['end_date_before_start'] =
+                "La date de fin de l\évènement doit se situer au minimum 1 jour après la date de début.
+                 Si l'évènement se déroule sur une seule journée, saisir uniquement la date de début";
+        }
+        return $errorsDate;
     }
 }
